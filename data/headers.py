cc8089b7c43efc09b1d91396ac171dc4dd8f54a6
deniz <denizcan_yilmaz@hotmail.com>
2025-01-12 19:22:39 +0300

    remove headers

--- data/headers.py
+++
@@ -1,4 +0,0 @@
-user_id,movie_id,rating,timestamp,year,month,day,age,gender,occupation,zip_code,movie_title,release_date,video_release_date,IMDb_URL,unknown,Action,Adventure,Animation,Children's,Comedy,Crime,Documentary,Drama,Fantasy,Film-Noir,Horror,Musical,Mystery,Romance,Sci-Fi,Thriller,War,Western
-
-
-244,51,2,1997-11-27 05:02:03,1997,11,27,28,M,technician,80525,Legends of the Fall (1994),01-Jan-1994,,http://us.imdb.com/M/title-exact?Legends%20of%20the%20Fall%20(1994),0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1
